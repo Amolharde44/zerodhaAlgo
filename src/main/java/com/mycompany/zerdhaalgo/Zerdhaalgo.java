94e30e53b056f2d6420d3842c229d01bac37c148
amol <amolharde98@gmail.com>
2024-01-23 08:56:46 +0530

    code

---
+++ src/main/java/com/mycompany/zerdhaalgo/Zerdhaalgo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zerdhaalgo;
+
+/**
+ *
+ * @author amolh
+ */
+public class Zerdhaalgo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
